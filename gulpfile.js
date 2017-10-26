25cbd3b7e7d9ede3015f0792e7eec40648bcf903
JIANGTAO-MA <1039142068@qq.com>
2017-10-26 13:39:39 +0800

    登录页
    未完待续

--- gulpfile.js
+++ gulpfile.js
@@ -2,14 +2,25 @@
  * Created by majiangtao on 2017/9/21.
  */
 var gulp = require('gulp');
-var less = require('gulp-less');
-var cssmin = require('gulp-minify-css');
+var less = require('gulp-less'); //less编译
+var uglify = require('gulp-uglify'); //js压缩
+var cssmin = require('gulp-minify-css'); //css压缩
+var htmlmin = require('gulp-minify-html'); //html压缩
+var imagemin = require('gulp-imagemin'); //图片压缩
+var jshint = require('gulp-jshint'); //js代码检查
+var concat = require('gulp-concat'); //文件合并
 var browserSync = require('browser-sync');
 
 gulp.task('default',['watch'], function () {
     console.log('默认任务');
 });
 
+//创建js编译任务，压缩合并
+gulp.task('minify-js', function () {
+    gulp.src('public/javascripts/main.js')
+        .pipe(uglify())
+        .pipe(gulp.dest('build/javascript'));
+});
 //创建less编译任务,编译后压缩css
 gulp.task('less', function () {
     gulp.src('public/less/*.less')
@@ -27,9 +38,10 @@ gulp.task('browserSync', function () {
 });
 
 var reload = browserSync.reload;
-gulp.task('watch',['less','browserSync'], function () {
+gulp.task('watch',['less','minify-js','browserSync'], function () {
     //添加监听事件
     gulp.watch('public/less/*.less',['less']);
+    gulp.watch('public/javascripts/*.js',['minify-js']);
 
     //根目录下任何变化涮新页面
     gulp.watch('build/**/*.*').on('change',reload);
--- routes/login.js
+++ routes/login.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET login page. */
 router.get('/', function(req, res, next) {
-  res.render('login', { title: '登录' });
+  res.render('login', { title: '登录',name: '圈子笔记' });
 });
 
 module.exports = router;
